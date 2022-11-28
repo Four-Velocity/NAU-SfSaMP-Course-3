ac233028a942982fde039bc7abcd796b51633bd1
Oleh Mazur <oleh@four-velocity.com>
2022-11-28 09:10:29 +0100

    :tada: Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<template class="main">
+  <v-toolbar color="primary">
+    <v-toolbar-title> Лабораторна робота №3 </v-toolbar-title>
+  </v-toolbar>
+</template>
+  
+  <style scoped>
+</style>
+  
+  <script>
+</script>
